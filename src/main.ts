4d9aae2c2caffef773c7e78f82bacbfdae51f702
nazrin ali <nali3171@outlook.com>
2025-01-19 18:18:22 +0000

    add quesion and array of answers and updating score

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,37 @@
-import './styles.scss'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-//import { setupCounter } from './counter.ts'
+import './styles.scss';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let currentQuestionIndex = 0;
+let score = 0;
+
+const questions: Question[] = [
+{
+  question: "What is the name of the world's tallest mountain?",
+  answers: ["Kilimanjaro", "Himalayas", "Mount Everest", "Barisan Mountains"],
+  correctAnswerIndex: 2,
+},
+];
+
+
+//Dom Elements
+
+
+//start game
+function startGame(){
+  currentQuestionIndex = 0;
+  score = 0;
+//updateUI();
+}
+
+
+function updateUI(){
+  const currentQuestion = questions[currentQuestionIndex];
+
+  questionElement.textContent = currentQuestion.question;
+
+
+  
+}
+
+
+
